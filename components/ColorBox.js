70ecb92cc311460500129c8e946511870d740949
Diego Ramos <diego.ramos@imagemaker.com>
2020-10-14 09:58:14 -0300

    Color Palette Application

---
+++ components/ColorBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+const ColorBox = ({colorName, hexCode}) => {
+  const boxColor = {
+    backgroundColor: hexCode,
+  };
+  const textStyle = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+  return (
+    <View style={[styles.box, boxColor]}>
+      <Text style={[styles.text, textStyle]}>
+        {colorName}: {hexCode}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+});
+
+export default ColorBox;
